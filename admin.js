64d023e987bbce1fb47cf73240b29e4eec8bdb2d
johnr111120-sys <johnr111120@gmail.com>
2026-02-25 13:22:41 -0300

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,60 @@
+  <script>
+let productos = JSON.parse(localStorage.getItem("productos")) || [];
+
+function cargarAdmin(){
+   const cont = document.getElementById("listaAdmin");
+   cont.innerHTML = "";
+
+   productos.forEach(p => {
+      cont.innerHTML += `
+         <div class="card-admin">
+            <img src="${p.imagen}" width="60">
+
+            <input value="${p.nombre}" 
+              onchange="editar('${p.id}','nombre',this.value)">
+
+            <input type="number" value="${p.precio}" 
+              onchange="editar('${p.id}','precio',this.value)">
+
+            <input type="number" value="${p.stock || 0}" 
+              onchange="editar('${p.id}','stock',this.value)">
+
+            <button onclick="eliminar('${p.id}')">Eliminar</button>
+         </div>
+      `;
+   });
+}
+
+function editar(id,campo,valor){
+   const p = productos.find(x=>x.id==id);
+   p[campo] = valor;
+   guardar();
+}
+
+function eliminar(id){
+   productos = productos.filter(p=>p.id!=id);
+   guardar();
+   cargarAdmin();
+}
+
+function nuevoProducto(){
+   const nuevo = {
+      id: Date.now(),
+      nombre: "Nuevo producto",
+      precio: 0,
+      imagen: "",
+      stock: 0
+   };
+
+   productos.push(nuevo);
+   guardar();
+   cargarAdmin();
+}
+
+function guardar(){
+   localStorage.setItem("productos", JSON.stringify(productos));
+}
+
+cargarAdmin();
+
+</script> 
