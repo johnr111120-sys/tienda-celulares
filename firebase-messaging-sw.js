b412c21bfa3db1b9240916bda377cd6d0b9f5764
johnr111120-sys <johnr111120@gmail.com>
2026-02-27 20:48:16 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBXzbZY5CRIM6M5LmjSiX6qFXf9cdloUg0",
-  authDomain: "admin-moises-celulares.firebaseapp.com",
-  projectId: "admin-moises-celulares",
-  storageBucket: "admin-moises-celulares.firebasestorage.app",
-  messagingSenderId: "657071812173",
-  appId: "1:657071812173:web:2c5522afdbd29fbb607604"
+  apiKey: "AIzaSyB0YrXAOE7eg04N6c3ZqLsBUUz8VBfHD58",
+  authDomain: "moises-celulares.firebaseapp.com",
+  projectId: "moises-celulares",
+  storageBucket: "moises-celulares.firebasestorage.app",
+  messagingSenderId: "392580256106",
+  appId: "1:392580256106:web:e31ae4c958effb4eb91513"
 });
 
 const messaging = firebase.messaging();
