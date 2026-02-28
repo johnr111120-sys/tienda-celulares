7990ae011847a942d781cce33c53c6fc9c229968
johnr111120-sys <johnr111120@gmail.com>
2026-02-27 23:58:33 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -11,16 +11,6 @@ onAuthStateChanged(auth, user=>{
 });
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB0YrXAOE7eg04N6c3ZqLsBUUz8VBfHD58",
-  authDomain: "moises-celulares.firebaseapp.com",
-  projectId: "moises-celulares",
-  appId: "1:392580256106:web:e31ae4c958effb4eb91513"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
 // 🔐 PROTEGER PANEL
 onAuthStateChanged(auth, (user) => {
   if (!user) {
